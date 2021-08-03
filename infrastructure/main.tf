9a271d4548dcd22d85ee0eee7330c8746cc1c9be
gavan <gavan.jon.lamb@gmail.com>
2021-08-03 18:29:06 +1000

    Set base domain for ACM ticket and wildcard for alternative domain

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,5 +1,7 @@
 ﻿resource "aws_acm_certificate" "wildcard" {
-  domain_name       = "*.${var.domain_name}"
+  domain_name = var.domain_name
+  subject_alternative_names = [
+    "*.${var.domain_name}" ]
   validation_method = "DNS"
 
   lifecycle {
